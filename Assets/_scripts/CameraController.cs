5cbd6e133b2428bba7cffac7b54312d5dc36d3b6
kw244 <kelvinwai@live.com.sg>
2015-08-26 11:44:14 +0800

    Roll A Ball Tutorial

---
+++ Assets/_scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// We use LateUpdate to ensure that the player object has already been updated for this frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
